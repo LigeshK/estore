3ce8fa66fe2ea2d6e1b7c4b626d1f7cf6ba87da9
Ligesh <ligeshkoshy@gmail.com>
2025-03-10 13:14:38 +1100

    Added side bar to show items category

---
+++ estore/src/Components/SideNav/index.js
@@ -0,0 +1,15 @@
+import './_side-nav.scss';
+
+const SideNav = ()=>{
+
+    return(
+        <div className='side-nav'>
+            <div className='section-title'>
+                <h3>Category</h3>
+            </div>
+            
+        </div>
+    )
+}
+
+export default SideNav;
