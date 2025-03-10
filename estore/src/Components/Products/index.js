c5ec0762cba2f48039d94460019729b2ec4d69e0
Ligesh <ligeshkoshy@gmail.com>
2025-03-10 15:18:50 +1100

    Added products card with dummy data and added styling

--- estore/src/Components/Products/index.js
+++ estore/src/Components/Products/index.js
@@ -1,20 +1,40 @@
-import './_products.scss';
+import "./_products.scss";
 
-const Products = ()=>{
-
-    return(
-        <div>
-            <div className='mx-5 p-3 col-lg-3 col-md-6 product-card'>
-                <div className='product-image-container'>
-                    <img src={require('../../assets/images/shop/shop-1.jpg')}/>
-                </div>
-                <div className='product-info'>
-                    <h5> <a href='#'>Jacket</a> </h5>
-                    <p className='product-price'> $45 </p>
-                </div>
+const Products = () => {
+  const productData = [
+    { pName: "Jacket", price: 45, img: "shop-1.jpg" },
+    { pName: "Purse", price: 50, img: "shop-2.jpg" },
+    { pName: "Dress", price: 69, img: "shop-3.jpg" },
+    { pName: "Denim", price: 30, img: "shop-4.jpg" },
+    { pName: "Boots", price: 49, img: "shop-5.jpg" },
+  ];
+  return (
+    <div>
+      {productData.map((product, key) => {
+        return (
+          <div className="mx-5 p-3 col-lg-3 col-md-6 product-card">
+            <div className="product-image-container">
+              <img
+                src={require("../../assets/images/shop/"+product.img)}
+                alt=""
+              />
             </div>
-        </div>
-    )
-}
+            <div className="product-info">
+              <h5><a href="#">{product.pName}</a></h5>
+              <p className="product-price"> ${product.price}</p>
+              <div className='product-rating'>
+                <i className='fa fa-star'/>
+                <i className='fa fa-star'/>
+                <i className='fa fa-star'/>
+                <i className='fa fa-star'/>
+                <i className='fa fa-star'/>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Products;
