91e583ea5a48736ba195f6e67aa071b8b112fabc
Ligesh <ligeshkoshy@gmail.com>
2025-07-02 20:26:31 +1000

    Established connection to mysql - estore database and get data with pool

--- estoreServer/index.js
+++ estoreServer/index.js
@@ -1,9 +1,29 @@
-const express = require("express");
-const app = express();
+const express = require("express"); // Importing express for server setup
+const app = express(); // Express server setup
+
+const mysql = require("mysql2"); // MySQL database connection
+
+// Create a connection pool for MySQL
+const pool = mysql.createPool({
+  host: "localhost", // Database host
+  user: "root", // Database user
+  password: "Dev@2025", // Database password
+  database: "estore", // Database name
+  port: 3306, // Database port
+  multipleStatements: true, // Allow multiple SQL statements in a single query
+});
 
 app.get("/", (req, res) => {
-  res.send("Hello from estoreServer1!");
-});
+  let categoryData;
+  pool.query("SELECT * FROM categories", (err, results) => {
+    if (err) {
+      res.status(500).send("Query execution failed: " + err.message);
+    } else {
+      categoryData = results;
+      res.status(200).send(categoryData); // Send the category data as JSON response
+    }
+  });
+}); // Endpoint to fetch categories
 
 const PORT = "5001";
 const server = app.listen(PORT, () => {
